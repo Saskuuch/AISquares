33c3fce1bf38ae4deb9965d5ff97331322be4d49
Ethan Warner <146154572+Saskuuch@users.noreply.github.com>
2024-04-19 13:23:14 -0700

    Update SadieUI.java

--- SadieUI.java
+++ SadieUI.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Work in progress UI, not functional at the moment, go to Algorithm.java for working algorithm
 package javafxapplication1;
 
 import java.awt.image.BufferedImage;
